2698c3a7cd257f6f50d2ce7b0cd779c69d44e4da
Eskizo2033 <j.esteban.rincon.m@gmail.com>
2021-11-29 11:41:40 +0100

    he añadido lo del menu a la sesion uno

--- Practica 3/src/sesion_1.java
+++ Practica 3/src/sesion_1.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class sesion_1 {
+    public static int menu(Scanner entrada){
+        int resultado = 0;
+        System.out.print("Menú Práctica 3:\n\b1. Insetar persona.\n\b2. Listar Personas\n\b0. Finalizar personas\nIntroduzca una opción del menú");
+        resultado = entrada.nextInt();
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        menu(entrada);
+    }
 }
