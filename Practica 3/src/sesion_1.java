86dcf92fb21380931fcd5454dbd556d6856eebcd
Eskizo2033 <j.esteban.rincon.m@gmail.com>
2021-11-30 09:59:14 +0100

    He creado todo lo que me pedían, ya he acabado la sesión uno

--- Practica 3/src/sesion_1.java
+++ Practica 3/src/sesion_1.java
@@ -8,9 +8,27 @@ public class sesion_1 {
         return resultado;
     }
 
+    public static Persona crearPersona(Scanner entrada){
+        Persona Persona1 = new Persona();
+        System.out.println("Nombre: ");
+        Persona1.setNombre(entrada.next());
+        System.out.println("Género (H/M): ");
+        Persona1.setGenero(entrada.next().charAt(0));
+        System.out.println("Edad: ");
+        Persona1.setEdad(entrada.nextInt());
+        System.out.println("Altura (m): ");
+        Persona1.setAltura(entrada.nextDouble());
+        System.out.println("Peso (kg): ");
+        Persona1.setPeso(entrada.nextDouble());
+        return Persona1;
+    }
+
     public static void main(String[] args) {
+        int decision;
         Scanner entrada = new Scanner(System.in);
-        menu(entrada);
-        Persona Persona = new Persona();
+        decision = menu(entrada);
+        if (decision == 1){
+            crearPersona(entrada);
+        }
     }
 }
